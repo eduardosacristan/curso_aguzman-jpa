a1bd0f548d372ee05563f2f190d6f75e8108cd7a
eduardosacristan <eduardosacristan@outlook.es>
2025-09-22 12:03:45 +0100

    added interface for application and first entity

--- src/main/java/com/andres/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/andres/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,13 +1,20 @@
 package com.andres.curso.springboot.jpa.springboot_jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJpaApplication {
+public class SpringbootJpaApplication implements CommandLineRunner{ //implementamos la interfaz porque no vamos a trabajar via web sino en linea de comandos
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+	//Con este metodo trabajaremos en consola
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
---
+++ src/main/java/com/andres/curso/springboot/jpa/springboot_jpa/entities/Person.java
@@ -0,0 +1,62 @@
+package com.andres.curso.springboot.jpa.springboot_jpa.entities;
+
+import org.springframework.boot.context.properties.bind.Name;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "personas") //Esto es opcional si la clase se llama igual que la tabla
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+    
+    @Name(value = "programming_language")
+    private String programminglanguage;
+
+    public Person() {
+        //Siempre que creamos un constructor personalizado deberemos crear un constructor vacío
+        //Hibernate lo necesita para mapear la clase
+    }
+    
+    public Person(Long id, String name, String lastname, String programminglanguage) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.programminglanguage = programminglanguage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getProgramminglanguage() {
+        return programminglanguage;
+    }
+    public void setProgramminglanguage(String programminglanguage) {
+        this.programminglanguage = programminglanguage;
+    }
+
+}
