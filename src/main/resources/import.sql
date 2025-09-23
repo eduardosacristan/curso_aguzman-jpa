8897a0b81dc7537e6dcc63a61e849431954a8cd2
eduardosacristan <eduardosacristan@outlook.es>
2025-09-23 16:15:56 +0100

    created data import for database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+    INSERT INTO personas (name, lastname, programming_language) VALUES ('Andres', 'Guzman', 'Java');
+    INSERT INTO personas (name, lastname, programming_language) VALUES ('Pepe', 'Doe', 'Python');
+    INSERT INTO personas (name, lastname, programming_language) VALUES ('John', 'Dow', 'JavaScript');
+    INSERT INTO personas (name, lastname, programming_language) VALUES ('Maria', 'Roe', 'Java');
+    INSERT INTO personas (name, lastname, programming_language) VALUES ('Josefa', 'Rae', 'Java');
